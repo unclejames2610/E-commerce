9f492867609c9a97f6fcf5c774f3ea4869b771e7
unclejames2610 <udealaebuka@gmail.com>
2023-03-16 17:33:17 +0100

    product

--- src/components/Product.js
+++ src/components/Product.js
@@ -20,16 +20,29 @@ const Product = ({ product }) => {
               alt=""
             />
           </div>
-          <div className="absolute top-0 right-0 bg-red-500 p-2">
+          <div className="absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
             <button>
-              <div className="flex justify-center items-center text-white w-12 h-12">
+              <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500">
                 <BsPlus className="text-3xl" />
               </div>
             </button>
+            <Link
+              to={`/product/${id}`}
+              className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl"
+            >
+              <BsEyeFill />
+            </Link>
           </div>
         </div>
       </div>
-      <div>2</div>
+      <div>
+        <div className="tetx-sm capitalize text-gray-500 mb-1">{category}</div>
+        <Link to={`/product/${id}`}>
+          <h2 className="font-semibold mb-1">{title}</h2>
+        </Link>
+
+        <div className="font-semibold">$ {price}</div>
+      </div>
     </div>
   );
 };
