f3af7aa3cc5fcf9003e6f4d38bec1aae58f9f864
unclejames2610 <udealaebuka@gmail.com>
2023-03-17 13:11:12 +0100

    brk

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IoMdClose, IoMdRemove, IoMdAdd } from "react-icons/io";
 
 const CartItem = ({ item }) => {
   //destructure item
@@ -10,9 +11,30 @@ const CartItem = ({ item }) => {
         <Link to={`/product/${id}`}>
           <img className="max-w-[80px]" src={image} alt="" />
         </Link>
-        <div>
-          <div>
-            <Link to={`/product/${id}`}>{title}</Link>
+        <div className="w-full flex flex-col">
+          <div className="flex justify-between mb-2">
+            <Link
+              to={`/product/${id}`}
+              className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+            >
+              {title}
+            </Link>
+            <div className="text-xl cursor-pointer">
+              <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
+            </div>
+          </div>
+          <div className="bg-pink-200 flex gap-x-2 h-[36px] text-sm">
+            <div className=" ">
+              <div>
+                <IoMdRemove />
+              </div>
+              <div>{amount}</div>
+              <div>
+                <IoMdAdd />
+              </div>
+            </div>
+            <div>item price</div>
+            <div>final price</div>
           </div>
         </div>
       </div>
