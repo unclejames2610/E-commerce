41d436388c4279e0c04030f91a4614e29a2da18f
unclejames2610 <udealaebuka@gmail.com>
2023-03-17 11:57:54 +0100

    cart-func

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -5,9 +5,27 @@ export const CartContext = createContext();
 const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
-  const addToCart = () => {
-    console.log("added to the cart");
+  const addToCart = (product, id) => {
+    const newItem = { ...product, amount: 1 };
+    //check if item is already in cart
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    //if cart item is already in the cart
+    if (cartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount + 1 };
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
   };
+  console.log(cart);
 
   return (
     <CartContext.Provider value={{ addToCart }}>
