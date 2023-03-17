ab25b4395063cb541dc4d8b0912bad4ed4b4303a
unclejames2610 <udealaebuka@gmail.com>
2023-03-17 17:54:53 +0100

    cartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -6,7 +6,7 @@ const CartItem = ({ item }) => {
   //destructure item
   const { id, title, image, price, amount } = item;
   return (
-    <div>
+    <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
       <div className="w-full min-h-[150px] flex items-center gap-x-4">
         <Link to={`/product/${id}`}>
           <img className="max-w-[80px]" src={image} alt="" />
@@ -23,18 +23,24 @@ const CartItem = ({ item }) => {
               <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
             </div>
           </div>
-          <div className="bg-pink-200 flex gap-x-2 h-[36px] text-sm">
-            <div className=" ">
-              <div>
+          <div className="flex gap-x-2 h-[36px] text-sm">
+            <div className="flex flex-1 max-w-[100px]  items-center h-full border text-primary font-medium ">
+              <div className="flex-1 flex justify-center items-center cursor-pointer">
                 <IoMdRemove />
               </div>
-              <div>{amount}</div>
-              <div>
+              <div className="h-full flex justify-center items-center px-2">
+                {amount}
+              </div>
+              <div className="flex-1 h-full flex justify-center items-center cursor-pointer">
                 <IoMdAdd />
               </div>
             </div>
-            <div>item price</div>
-            <div>final price</div>
+            <div className="flex-1 flex items-center justify-around">
+              $ {price}
+            </div>
+            <div className="flex-1 flex justify-end items-center text-primary font-medium">{`$ ${parseFloat(
+              price * amount
+            ).toFixed(2)}`}</div>
           </div>
         </div>
       </div>
