244fc61f9131b9fa38a901e746f6c915abec11b5
unclejames2610 <udealaebuka@gmail.com>
2023-03-20 17:56:58 +0100

    productdetails

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import WomanImg from "../img/woman_hero.png";
 
 const Hero = () => {
   return (
-    <section className="bg-pink-200 h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
+    <section className=" h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
       <div className="container mx-auto flex justify-around h-full">
         <div className="flex flex-col justify-center">
           <div className="font-semibold flex items-center uppercase">
